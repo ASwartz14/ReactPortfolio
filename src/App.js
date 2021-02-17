6bc6b03c94b23acf9b16ca840ed6e8b26136cf18
ASwartz14 <asriley144@gmail.com>
2021-02-17 12:45:56 -0800

    tweaking

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div class="background-img"></div>
       <div>
         <NavTabs />
-        <Route exact path="/" component={About} />
+        <Route exact path="/ReactPortfolio" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
